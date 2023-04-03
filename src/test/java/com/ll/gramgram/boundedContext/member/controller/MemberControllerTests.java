5c9c7a52f650e0913e36d53e5e5d308f4cbfabe6
parkchaewon18 <0101angela@naver.com>
2023-04-03 15:57:34 +0900

    회원 가입 폼 구현

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-@ActiveProfiles("test")
+@SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야 함, Ioc 컨테이너 작동시킴
+@AutoConfigureMockMvc // http 요청, 응답 테스트
+@Transactional // 실제로 테스트에서 발생한 DB 작업이 영구적으로 적용되지 않도록, test + 트랜잭션 => 자동롤백
+@ActiveProfiles("test") // application-test.yml 을 활성화 시킨다.
 public class MemberControllerTests {
     @Autowired
     private MockMvc mvc;
@@ -31,13 +32,22 @@ public class MemberControllerTests {
         // WHEN
         ResultActions resultActions = mvc
                 .perform(get("/member/join"))
-                .andDo(print());
+                .andDo(print()); // 크게 의미 없고, 그냥 확인용
 
         // THEN
         resultActions
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("showJoin"))
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("""
+                        <input type="text" name="username"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="password" name="password"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="submit" value="회원가입"
+                        """.stripIndent().trim())));
     }
 
     @Test
@@ -46,7 +56,7 @@ public class MemberControllerTests {
         // WHEN
         ResultActions resultActions = mvc
                 .perform(post("/member/join")
-                        .with(csrf())
+                        .with(csrf()) // CSRF 키 생성
                         .param("username", "user10")
                         .param("password", "1234")
                 )
@@ -57,6 +67,5 @@ public class MemberControllerTests {
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("join"))
                 .andExpect(status().is3xxRedirection());
-
     }
 }
